29a2eea4f8648793dc02b9ea3e0f0f40b1b6bffb
joelsprangers <68640121+joelsprangers@users.noreply.github.com>
2020-11-16 11:12:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if(age=>18){
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Je bent helaas te jong.");
+}
+
+if(isFemale){
+    console.log("Je mag naar ladiesnight!");
+} else {
+    console.log("Je mag helaas niet naar ladiesnight");
+}
+
+if(driverStatus=="bob"){
+    console.log("Je bent de bob en mag rijden!");
+} else {
+    console.log("Je bhebt gedronken en moet een Uber bestellen...");
+}
